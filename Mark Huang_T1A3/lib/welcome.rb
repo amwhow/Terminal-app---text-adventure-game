48c36099ca4c5a320f7de03d6559fbc383f1ba85
Mark <amwhow@gmail.com>
2020-09-29 12:46:29 +1000

    changed all class methods into instance methods, setup the start game command at the bottom

--- Mark Huang_T1A3/lib/welcome.rb
+++ Mark Huang_T1A3/lib/welcome.rb
@@ -1,29 +1,34 @@
 # frozen_string_literal: true
+
 require 'tty-prompt'
 require 'io/console'
 require 'colorize'
-require_relative 'story.rb'
-require_relative 'text_display.rb'
+require_relative 'story'
+require_relative 'text_display'
 
-# Welcome menu function 
+# Welcome menu function
 class Welcome
-extend TextLayout
+  include TextLayout
 
   def initialize
-    Welcome.clear_screen
-    @prompt = TTY::Prompt.new
-    user_welcome_choice = @prompt.select('*** Welcome to the great text adventure game - Jungle Run! ***', %w[Newgame Load Exit])
+    @story = Story.new
+  end
+
+  def menu
+    clear_screen
+    prompt = TTY::Prompt.new
+    user_welcome_choice = prompt.select('*** Welcome to the great text adventure game - Jungle Run! ***', %w[Newgame Load Exit])
     # sleep(0.5)
     # system("afplay ./lib/Kalimdor.mp3")
     case user_welcome_choice
     when 'Newgame'
-      Welcome.clear_screen
+      clear_screen
       newgame_welcome
     when 'Load'
-      Welcome.clear_screen
+      clear_screen
       load_page
     when 'Exit'
-      Welcome.clear_screen
+      clear_screen
       puts 'The jungle is always here for you, so long.'
       exit
     end
@@ -32,24 +37,25 @@ extend TextLayout
   # Newgame prompts
   def newgame_welcome
     puts 'Presented by: Mark Huang'.center(100)
-    sleep(3)
-    Welcome.clear_screen
+    # sleep(3)
+    clear_screen
     print 'Welcome to the Jungle, adventurer'
     puts '(Press any key to continue)'
-    Welcome.next_line
+    next_line
     puts 'May I know your name?'
     print '>'
     player_name = gets.chomp
-    Welcome.clear_screen
+    clear_screen
     puts "Greetings, #{player_name}. Your Jungle journey starts now..."
-    Welcome.next_line
-    Welcome.clear_screen
-    Story.story_one
+    next_line
+    clear_screen
+    @story.story_one
   end
 
   # Load your save data
   def load_page
-    user_load_choice = @prompt.select('*** Please select your save slot ***', %w[Save1 Save2 Save3])
+    prompt = TTY::Prompt.new
+    user_load_choice = prompt.select('*** Please select your save slot ***', %w[Save1 Save2 Save3])
     case user_load_choice
     when 'Save1'
       puts 'this is Save1'
@@ -61,4 +67,5 @@ extend TextLayout
   end
 end
 
-Welcome.new
+play = Welcome.new
+play.menu

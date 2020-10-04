ac42fec46ae318fbb1e6b97dc3ff026686521277
Mark <amwhow@gmail.com>
2020-10-04 20:22:17 +1100

    final adjustments done, ready to submit

--- Mark Huang_T1A3/lib/run.rb
+++ Mark Huang_T1A3/lib/run.rb
@@ -8,3 +8,14 @@ require_relative 'battle.rb'
 
 @@play = Welcome.new
 @@play.menu
+
+# def help_docs 
+#   puts "Hitting any key to continue the story."
+#   puts "Making choices from the prompt menu."
+#   puts "Follow the in-game instruction and enjoy your journey!"
+# end
+
+# if ARGV[0] == "-h" || ARGV[0] == "--help"
+#   help_docs
+#   exit
+# end
